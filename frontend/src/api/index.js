6106c8e4b8188f71ea008c0b4c5347fac781c30b
Achraf-haddar <achraf.haddar@supcom.tn>
2022-01-01 12:25:55 +0100

    update

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    //baseURL: 'http://localhost:3000/api',  // For local development
+    baseURL: '/api' // For Production
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
